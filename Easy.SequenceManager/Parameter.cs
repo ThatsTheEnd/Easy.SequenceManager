5a5a8f0dd07a645bf9c38a37e061b4f403f2e8fc
Nikolaus Naredi-Rainer <nikolaus.naredi-rainer@zuehlke.com>
2024-01-03 17:01:39 +0100

    refactoring - everything still working

---
+++ Easy.SequenceManager/Parameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy.SequenceManager
+{
+    /// <summary>
+    /// This is a dataclass to hold information about a parameter. It is used in the Step class within a list.
+    /// </summary>
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public bool Required { get; set; }
+        public string DefaultValue { get; set; }
+        public string HooverOverInfo { get; set; }
+    }
+}
--- Easy.SequenceManager/SequenceManager.cs
+++ Easy.SequenceManager/SequenceManager.cs
@@ -327,18 +327,5 @@ namespace Easy.SequenceManager
     }
 
 
-    /// <summary>
-    /// This is a dataclass to hold information about a parameter. It is used in the Step class within a list.
-    /// </summary>
-    public class Parameter
-    {
-        public string Name { get; set; }
-        public string Type { get; set; }
-        public string Value { get; set; }
-        public bool Required { get; set; }
-        public string DefaultValue { get; set; }
-        public string HooverOverInfo { get; set; }
-    }
-
 }
 
